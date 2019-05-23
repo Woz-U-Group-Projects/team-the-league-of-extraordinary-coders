081adc16185be91a91107802d3390e4283cf7d9e
Daniel Fabian Brent <danielbrent@gmail.com>
2019-05-23 14:14:05 -0400

    Add files via upload

---
+++ LOEC/src/main/java/com/example/LOEC/LoecApplication.java
@@ -0,0 +1,13 @@
+package com.example.LOEC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoecApplication.class, args);
+	}
+
+}
