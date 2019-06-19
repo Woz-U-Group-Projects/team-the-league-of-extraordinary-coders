745deefb585480d7b2d310fee1a107db74e01906
Roje93 <42291111+Roje93@users.noreply.github.com>
2019-06-19 13:20:15 -0700

    Add files via upload

---
+++ LOEC/src/main/java/com/example/LOEC/Contacts.java
@@ -0,0 +1,27 @@
+package com.contact.models;
+
+public class Contacts {
+	private int contactId;
+	private String contactTitle;
+	private int contactEmployeeId;
+	
+	public int getContactId() {
+		return contactId;
+	}
+	public void setAlbumId(int contactId) {
+		this.contactId = contactId;
+	}
+	public String getContactTitle() {
+		return contactTitle;
+	}
+	public void setContactTitle(String contactTitle) {
+		this.contactTitle = contactTitle;
+	}
+	public int getcontactContactId() {
+		return contactEmployeeId;
+	}
+	public void setcontactEmployeeId(int contactEmployeeId) {
+		this.contactEmployeeId = contactEmployeeId;
+	}
+
+}
